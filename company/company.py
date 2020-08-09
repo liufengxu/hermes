34dde7dc417e27b8ee7f7be5f5473d4501ff667b
liufengxu <onx0320@126.com>
2020-08-09 23:55:32 +0800

    add person

--- company/company.py
+++ company/company.py
@@ -1,3 +1,12 @@
 class Company(object):
     def __init__(self, name):
         self.name = name
+
+    def set_domain(self, domain):
+        self.domain = domain
+
+    def set_year(self, year):
+        self.year = year
+
+    def set_value(self, value):
+        self.value = value
---
+++ company/person.py
@@ -0,0 +1,3 @@
+class Person(object)：
+    def __init__(self, name):
+        self.name = name
